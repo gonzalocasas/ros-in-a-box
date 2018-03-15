7c766e9ccb36db49ec9b2008221806cd72e1c98e
Gonzalo Casas <gonzalocasas@gmail.com>
2018-03-15 23:38:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,4 +38,12 @@ if [[ $NEW_HOSTNAME != $CURRENT_HOSTNAME ]]; then
   #sed -i "s/$CURRENT_HOSTNAME/$NEW_HOSTNAME/" /etc/hosts
 fi
 
+echo "Updating locale..."
+update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
+
+echo "Updating apt-get and adding ROS sources..."
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+apt-get update
+
 echo "Done"
