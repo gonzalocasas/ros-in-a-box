d7cb0e7d14e28706a4ca85ec70a9dbada39990d8
Gonzalo Casas <gonzalocasas@gmail.com>
2018-03-16 02:02:12 +0000

    Add ros-control

--- install-1.sh
+++ install-1.sh
@@ -24,6 +24,7 @@ rosdep update
 echo "Installing ROS Bridge and additional tools..."
 sudo apt-get install -y ros-kinetic-rosbridge-server ros-kinetic-tf2-web-republisher
 sudo apt-get install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt-get install -y ros-kinetic-ros-control ros-kinetic-ros-controllers
 
 # Add ROS sourcing to profile
 grep -qF 'source /opt/ros/kinetic/setup.bash' ~/.bashrc || echo 'source /opt/ros/kinetic/setup.bash' >> ~/.bashrc
